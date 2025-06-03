7d87a55acddc0822ca48aec3c029e08b46dc1c73
kobayashi kyohei <kobayak@outlook.jp>
2025-06-03 20:29:41 +0900

    add two-sum solution

---
+++ Leetcode/two_sum/two_sum_hash_two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       table={}
+       for i in range(len(nums)):
+            need=target-nums[i]
+            if need in table:
+                return[i,table[need]]    
+            table[nums[i]]=i
+            #早い。優れた方法。O(n)の速度
+#tableのキーに元の配列の値を入れて、値にキーを入れるのがミソ#
